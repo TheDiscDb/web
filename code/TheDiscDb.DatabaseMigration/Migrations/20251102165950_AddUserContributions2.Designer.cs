06c3ea471836b21edcfa0ccc87ffb8540287e0cf
Luke Foust <luke@foust.com>
2025-11-02 09:01:54 -0800

    Move away from github packages to just nuget

--- code/TheDiscDb.DatabaseMigration/Migrations/20251102165950_AddUserContributions2.Designer.cs
+++ code/TheDiscDb.DatabaseMigration/Migrations/20251102165950_AddUserContributions2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheDiscDb.Web.Data;
 
@@ -11,9 +12,11 @@ using TheDiscDb.Web.Data;
 namespace TheDiscDb.Web.Migrations
 {
     [DbContext(typeof(SqlServerDataContext))]
-    partial class SqlServerDataContextModelSnapshot : ModelSnapshot
+    [Migration("20251102165950_AddUserContributions2")]
+    partial class AddUserContributions2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ code/TheDiscDb.DatabaseMigration/Migrations/20251102165950_AddUserContributions2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheDiscDb.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserContributions2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Tracks");
+        }
+    }
+}
