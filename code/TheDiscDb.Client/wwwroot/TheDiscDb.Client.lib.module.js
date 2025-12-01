694085b5841403b34f111030f4e7fd2eb8d322a0
Luke Foust <luke@foust.com>
2025-11-30 21:15:13 -0800

    Apply workaround for Blazor loading issue in .net 10

---
+++ code/TheDiscDb.Client/wwwroot/TheDiscDb.Client.lib.module.js
@@ -0,0 +1,4 @@
+﻿// This is a workaround until this issue gets fixed: https://github.com/dotnet/aspnetcore/issues/64009
+export function onRuntimeConfigLoaded(config) {
+    config.disableNoCacheFetch = true;
+}
