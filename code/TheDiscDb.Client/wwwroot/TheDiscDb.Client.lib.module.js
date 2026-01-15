5eafd61950d73d5209666fdccc7658d30ab5ff49
Luke Foust <luke@foust.com>
2026-01-14 18:31:27 -0800

    Remove workaround for loading issue

--- code/TheDiscDb.Client/wwwroot/TheDiscDb.Client.lib.module.js
+++
@@ -1,4 +0,0 @@
-﻿// This is a workaround until this issue gets fixed: https://github.com/dotnet/aspnetcore/issues/64009
-export function onRuntimeConfigLoaded(config) {
-    config.disableNoCacheFetch = true;
-}
