df1e11c6f57330d72fe40b5a91aeb8b11527a7bd
Luke Foust <luke@foust.com>
2026-01-24 18:50:10 -0800

    Turn off checking my contributions on main contribution page

--- code/TheDiscDb.Client/Pages/Contribute/Index.razor.cs
+++ code/TheDiscDb.Client/Pages/Contribute/Index.razor.cs
@@ -15,23 +15,23 @@ public partial class Index : ComponentBase
 
     public IQueryable<IMyPendingContributions_MyContributions_Nodes>? PendingContributions { get; set; }
 
-    protected override async Task OnInitializedAsync()
-    {
-        if (this.AuthProvider != null)
-        {
-            var state = await this.AuthProvider.GetAuthenticationStateAsync();
-            if (state != null)
-            {
-                var result = await this.ContributionClient.MyPendingContributions.ExecuteAsync();
-                if (result != null && result.IsSuccessResult())
-                {
-                    var pendingContributions = result.Data!.MyContributions!.Nodes!;
-                    if (pendingContributions != null && pendingContributions.Any())
-                    {
-                        this.PendingContributions = pendingContributions.AsQueryable();
-                    }
-                }
-            }
-        }
-    }
+    //protected override async Task OnInitializedAsync()
+    //{
+    //    if (this.AuthProvider != null)
+    //    {
+    //        var state = await this.AuthProvider.GetAuthenticationStateAsync();
+    //        if (state != null)
+    //        {
+    //            var result = await this.ContributionClient.MyPendingContributions.ExecuteAsync();
+    //            if (result != null && result.IsSuccessResult())
+    //            {
+    //                var pendingContributions = result.Data!.MyContributions!.Nodes!;
+    //                if (pendingContributions != null && pendingContributions.Any())
+    //                {
+    //                    this.PendingContributions = pendingContributions.AsQueryable();
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 }
