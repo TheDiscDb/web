a15adea8d7bc41f6f60567db055b69b50b4b4b01
Luke Foust <luke@foust.com>
2026-04-12 17:54:13 -0700

    Update webapp name in deployment

--- code/TheDiscDb.Naming/FileNameSanitizer.cs
+++ code/TheDiscDb.Naming/FileNameSanitizer.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 
 namespace TheDiscDb.Naming;
 
 public static class FileNameSanitizer
 {
-    private static readonly HashSet<char> IllegalChars = new(Path.GetInvalidFileNameChars());
+    // Use an explicit set of characters that are illegal in Windows file names,
+    // regardless of the current OS, so sanitized names are portable.
+    private static readonly HashSet<char> IllegalChars = new(
+        ['\"', '<', '>', '|', '\0', '\u0001', '\u0002', '\u0003', '\u0004', '\u0005',
+         '\u0006', '\u0007', '\b', '\t', '\n', '\u000b', '\f', '\r', '\u000e', '\u000f',
+         '\u0010', '\u0011', '\u0012', '\u0013', '\u0014', '\u0015', '\u0016', '\u0017',
+         '\u0018', '\u0019', '\u001a', '\u001b', '\u001c', '\u001d', '\u001e', '\u001f',
+         ':', '*', '?', '\\', '/']);
 
     /// <summary>
     /// Sanitizes a string for use as a filename by replacing or removing
